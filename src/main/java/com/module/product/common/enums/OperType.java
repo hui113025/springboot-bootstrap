68d6f1a87ed6f502cfd7f94fce2a6133a6dbe36d
hui113025 <zhenghui@duia.com>
2017-08-03 17:37:43 +0800

    springboot-bootstrap 后台管理系统

--- src/main/java/com/module/product/common/enums/OperType.java
+++ src/main/java/com/module/product/common/enums/OperType.java
@@ -7,14 +7,8 @@ package com.module.product.common.enums;
 public enum OperType {
     SYSTEM("系统操作","system"),
     AUTH("授权操作","auth"),
-    CATE("分类操作","CATE"),
     FEEDBACK("意见反馈操作","FEEDBACK"),
-    STUDENT("学员操作","student"),
-    MESSAGE("消息操作","message"),
-    EXAMINE("充值操作","examine"),
-    CHANNEL("渠道操作","channel"),
-    TIKU("渠道操作","tiku"),
-    COUPON("卡券操作","coupon");
+    MESSAGE("消息操作","message");
 
     OperType(String name,String value) {
         this.name = name;
--- src/main/java/com/module/product/orm/mapper/CouponMapper.java
+++
@@ -1,7 +0,0 @@
-package com.module.product.orm.mapper;
-
-import com.module.product.orm.core.Mapper;
-import com.module.product.orm.model.Coupon;
-
-public interface CouponMapper extends Mapper<Coupon> {
-}
